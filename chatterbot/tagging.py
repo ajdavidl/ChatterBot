c2a39ad42ab7f44085de37a945e6159d3ab621cf
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-03-24 10:28:59 -0300

    update taggin.py for spacy compatibility in other languages.

--- chatterbot/tagging.py
+++ chatterbot/tagging.py
@@ -26,7 +26,7 @@ class PosLemmaTagger(object):
         if self.language.ISO_639_1.lower() == 'en':
             self.nlp = spacy.load('en_core_web_sm')
         else:
-            self.nlp = spacy.load(self.language.ISO_639_1.lower())
+            self.nlp = spacy.load(self.language.ISO_639_1.lower()+'_core_news_sm')
 
     def get_text_index_string(self, text):
         """
